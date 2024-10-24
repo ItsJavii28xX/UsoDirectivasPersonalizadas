017640dc23101a3e01737c959f90bad60841163d
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-10-24 17:26:30 +0200

    AhoraDirectivaConParametro

--- src/app/custom-link-hover.directive.ts
+++ src/app/custom-link-hover.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[hoverDirective]',
   standalone: true
 })
 export class CustomLinkHoverDirective {
+  
+  // El nombre del Input es el mismo que el de la directiva
+  @Input('hoverDirective') hoverDirective!: { borderColor: string };
 
   constructor(private element: ElementRef) {}
 
   @HostListener('mouseover') mouseOver() {
-    this.element.nativeElement.classList.add('custom');
+    this.element.nativeElement.style.border = `2px solid ${this.hoverDirective.borderColor}`;
   }
 
   @HostListener('mouseout') mouseOut() {
-    this.element.nativeElement.classList.remove('custom');
+    this.element.nativeElement.style.border = 'none';
   }
 }
-
